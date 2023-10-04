d70aba287cd07ed611bdbdd53b3c7795f0476a5f
RakhmetMakhashev <rakhmet.m@digital-spectr.ru>
2023-10-04 21:15:35 +0300

    add ip

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    'telega_bot',
+    'tg_bot',
     'root',
     'root',
     {
-        host: '5.188.128.98',
+        host: '5.188.129.91',
         port: '6432',
         dialect: 'postgres'
     }
